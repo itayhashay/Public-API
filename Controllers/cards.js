fa9bc11534c46f998d570613de2fbfa9b1566942
Isar_NS <thxr0654@gmail.com>
2022-10-13 19:26:21 +0300

    add navbar ejs

--- Controllers/cards.js
+++ Controllers/cards.js
@@ -33,6 +33,36 @@ let cards_mocks = [
         userImg: "img/pitbull2.jpg",
         userProfileUrl: "https://pitbull.io"
     },
+    {
+        id: "1",
+        img: "img/GerbilCropped.png",
+        title: "API 1",
+        desc: "BLAHHHHHHHHHHHHHjfhgdkjfkjfdsghkjfdshgkjfdshgksjdhgkjsfdhgkjshdgkjhsfgkjhsfdkjgh jdsfhds dskjfh skjdfh jskhdf kjdshf ksjdhf kjdshf",
+        upvotes: 45,
+        article: "https://google.com",
+        userImg: "img/GerbilCropped.png",
+        userProfileUrl: "gerbils.io"
+    },
+    {
+        id: "2",
+        img: "img/pitbull2.jpg",
+        title: "PITBULLLLLLLLL",
+        desc: "YEAHHHHHHHHHHHHHHHHHHH",
+        upvotes: 69,
+        article: "https://pitbull.com",
+        userImg: "img/pitbull1.jpg",
+        userProfileUrl: "https://pitbull.io"
+    },
+    {
+        id: "3",
+        img: "img/pitbull1.jpg",
+        title: "PITBULLLLLLLLL",
+        desc: "YEAHHHHHHHHHHHHHHHHHHH \n the \n line \n is new \n cant see me ",
+        upvotes: 6969,
+        article: "https://pitbull.com",
+        userImg: "img/pitbull2.jpg",
+        userProfileUrl: "https://pitbull.io"
+    },
 ]
 
 
@@ -42,7 +72,7 @@ module.exports = function(app) {
         res.render('Cards',
         { 
             cards: cards_mocks,
-            layout: 'Layouts/bootstrap.ejs'
+            layout: 'Layouts/navbar.ejs'
         }
         )
     })
