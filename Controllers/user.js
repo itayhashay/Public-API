bd9d05eab5da9f0f6ebbc4cd05e4b156bd1be27b
itayhashay <itay0806@gmail.com>
2022-10-21 13:59:04 +0300

    delete passwords from responds of users controller

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,28 +1,38 @@
 const express = require('express'),
+  hash = require('object-hash'),
   User = require('../Models/user'),
   Bookmark = require('../Models/bookmark'),
   router = express.Router();
 
 router.get('/', async (req, res) => {
   const users = await User.find({});
-  res.send({ data: users })
+  for (let i = 0; i < users.length; i++) {
+    delete users[i]['_doc']["password"];
+  }
+  res.send({ data: users });
 })
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const user = await User.findById(id);
+  delete user['_doc']["password"];
   res.send({ data: user })
 })
 
 router.get('/search', async (req, res) => {
   let { query } = req;
   const user = await User.find({ query });
+  for (let i = 0; i < users.length; i++) {
+    delete users[i]['_doc']["password"];
+  }
   res.send({ data: user })
 })
 
 router.post('/', async (req, res) => {
+  req.body.password = hash(req.body.password);
   const newUser = new User(req.body);
   await newUser.save();
+  delete newUser['_doc']["password"];
   res.send({ data: newUser });
 })
 
@@ -30,13 +40,15 @@ router.post('/:id', async (req, res) => {
   const { id } = req.params;
   const user = await User.findByIdAndUpdate(id, req.body, { new: true });
   await user.save();
+  delete user['_doc']["password"];
   res.send({ data: user });
 })
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   const user = await User.findByIdAndDelete(id);
-  const bookmark = await Bookmark.deleteMany({ userId: user._id });
+  await Bookmark.deleteMany({ userId: user._id });
+  delete user['_doc']["password"];
   res.send({ data: user });
 })
 
