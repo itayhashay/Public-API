9298067da768f736ee29c07b46df1a9493275928
itayhashay <itay0806@gmail.com>
2022-10-22 00:37:43 +0300

    upvote, dynamic search and category type

--- Controllers/api.js
+++ Controllers/api.js
@@ -9,25 +9,38 @@ router.get('/', async (req, res) => {
   res.send({ data: apis })
 })
 
+router.get('/search', async (req, res) => {
+  let { q } = req.query;
+  const api = await Api.find({
+    $or: [{ name: { $regex: q, $options: 'i' } }, { description: { $regex: q, $options: 'i' } }, { url: { $regex: q, $options: 'i' } }, { category: { $regex: q, $options: 'i' } }]
+  });
+  res.send({ data: api });
+})
+
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const api = await Api.findById(id);
-  res.send({ data: api })
-})
-
-router.get('/search', async (req, res) => {
-  let { query } = req;
-  const api = await Api.find({ query })
-  res.send({ data: api })
+  res.send({ data: api == null ? [] : api })
 })
 
 router.post('/', async (req, res) => {
+  // const category = await Category.findOne({ name: req.body.category })
+  // req.body.category = category.name;
   const newApi = new Api(req.body);
   await newApi.save();
   res.send({ data: newApi });
 })
 
-router.post('/:id', async (req, res) => {
+router.post('/upvote/:id', async (req, res) => {
+  const { id } = req.params;
+  let api = await Api.findById(id);
+  api.upvote++;
+  api = await Api.findByIdAndUpdate(id, api, { new: true });
+  api.save();
+  res.send({ data: api })
+})
+
+router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const api = await Api.findByIdAndUpdate(id, req.body, { new: true });
   await api.save();
@@ -37,7 +50,7 @@ router.post('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   const api = await Api.findByIdAndDelete(id);
-  const bookmark = await Bookmark.deleteMany({ apiId: api._id });
+  await Bookmark.deleteMany({ apiId: api._id });
   res.send({ data: api });
 })
 
