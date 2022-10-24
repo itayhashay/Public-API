1caa99f376d90b801d80c83a5cbca4db54e3a0a7
Isar_NS <thxr0654@gmail.com>
2022-10-24 20:32:03 +0300

    upvote test

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     $('.upvote').each(function () {
         var $this = $(this);
         $this.on("click", function () {
-            console.log($(this).data('id'));
             var id = $(this).data('id')
+            console.log('/api/upvote/'+id);
             $.ajax( {
                 url: '/api/upvote/'+id, // here you put the url path that will retrive the page
                 type: 'POST',
