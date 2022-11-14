6939d5602228634021ee25765fe150e80e9db4f4
Isar_NS <thxr0654@gmail.com>
2022-11-15 00:30:47 +0200

    finish search

--- public/js/search.js
+++ public/js/search.js
@@ -3,35 +3,15 @@ const name      = document.getElementById('Checkbox-name');
 const category  = document.getElementById('Checkbox-category');
 const upload_by = document.getElementById('Checkbox-uploadby');
 
-const inputHandler = function(e) {
-  //result.innerHTML = e.target.value;
-  console.log(source.value)
-  console.log(name.checked)
 
-  $.ajax({
-    url: '/api/search?text='+source.value + "&name=" + name.checked + "&category=" + category.checked + "&uploadby=" + upload_by.checked, // here you put the url path that will retrive the page
-    type: 'GET',
-    contenttype: 'application/json',
-    dataType: "json",
-    data: {},
-    success: ({ data }) => {
-        console.log(data)
-        $.ajax({
-            url: '/', // here you put the url path that will retrive the page
-            type: 'GET',
-            contenttype: 'application/json',
-            dataType: "json",
-            data: {},
-            success: ({ data }) => {
-                location.href = '/';
-            }
-            })
-        }
-    })
-
-}
-
-source.addEventListener('input', inputHandler);
-source.addEventListener('propertychange', inputHandler); // for IE8
-// Firefox/Edge18-/IE9+ don’t fire on <select><option>
-// source.addEventListener('change', inputHandler); 
+source.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+        console.log(source.value)
+        console.log(name.checked)
+        location.href = '/?text='+source.value + "&name=" + name.checked + "&category=" + category.checked + "&uploadby=" + upload_by.checked;
+    }
+  });
