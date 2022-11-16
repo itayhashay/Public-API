7599c001bf6e2a3c8eb28614271420662b15b1e8
itayhashay <itay0806@gmail.com>
2022-11-16 20:06:27 +0200

    sign up page

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -5,6 +5,48 @@ $(document).ready(function () {
   $("#close").click(function () {
     $(".model-container").css("transform", "scale(0)");
   });
+
+  $('#sign-up-send').unbind('click').click(function () {
+    debugger;
+    const username = document.getElementById("username").value;
+    const firstName = document.getElementById("firstname").value;
+    const lastName = document.getElementById("lastname").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const gender = document.getElementById("gender").value == 1 ? "Male" : "Female";
+    const birthday = document.getElementById("birthday").value;
+    data = {
+      username,
+      firstName,
+      lastName,
+      password,
+      email,
+      gender,
+      birthday
+    }
+    console.log(data);
+    $.ajax({
+      url: "/user", // here you put the url path that will retrive the page
+      type: "POST",
+      contenttype: "application/json",
+      dataType: "json",
+      data: {
+        username,
+        firstName,
+        lastName,
+        password,
+        email,
+        gender,
+        birthday
+      },
+      success: ({ data }) => {
+        console.log(data);
+        location.assign("/login");
+      }
+    });
+    return false;
+    //addToCart($(this).attr("id"));
+  });
 });
 
 //material contact form
